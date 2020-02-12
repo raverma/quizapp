19371da35c669cc996ea9630fa5ef89ee8d32027
tarunsharma89 <TS00453995@techmahindra.com>
2020-02-12 12:05:13 +0530

    Create routes.ts

---
+++ src/app/routes.ts
@@ -0,0 +1,18 @@
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+
+import { UserComponent } from './user/user.component';
+
+import { Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+  {
+    path: 'signup', component: SignUpComponent
+    
+},
+{
+    path: 'login', component: SignInComponent
+    
+},
+  ];
+
